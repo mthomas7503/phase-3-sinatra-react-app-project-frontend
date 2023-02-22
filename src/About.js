46d49bbd1d05c941feebe5e8dd04454d118ee1ec
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2023-02-21 19:43:48 -0500

    Add NavBar and update other components

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About () {
+    return (
+        <div>
+            <h2>
+                React Verson: 18.2.0
+                React-router Verson: 6.8.1
+                Final Fantasy Info: <a target="_blank" href="https://finalfantasy.fandom.com/wiki/Final_Fantasy_Wiki">Final Fantasy Wiki</a>
+            </h2>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return(
+        <div>
+            <h1> Welcome to Monsters 'R Us! Your guide to the monsters of Final Fantasy 6!</h1>
+        </div>
+    )
+}
+
+export default Home;
