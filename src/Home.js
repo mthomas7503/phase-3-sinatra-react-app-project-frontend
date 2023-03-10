f80de27c46496f82cdb3085f70ae596361ce2487
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2023-03-09 19:15:40 -0500

    update css for home page

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
     return(
-        <div>
+        <div id='home'>
             <h1> Welcome to Monsters 'R Us! Your guide to the monsters of Final Fantasy 6!</h1>
         </div>
     )
